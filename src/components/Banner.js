d65536c391dbc71568c3124ebf15642af3f2309b
Son Mac <mactranthienson@gmail.com>
2022-01-16 00:06:47 -0700

    Updated banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,10 +3,10 @@ import React from 'react';
 const Banner = () => {
   return (
     <div className='mb-3 welcome'>
-      <h1 className='welcome-sentence'>NASALLERY</h1>
-      <p className='intro-para'>
+      <h1 className='site-name'>NASALLERY</h1>
+      {/* <p className='intro-para'>
         An astronomy art a day keeps the doctor away!
-      </p>
+      </p> */}
     </div>
   );
 };
